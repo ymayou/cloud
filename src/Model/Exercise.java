d34321770de3f95017b02a016a40e0985f3a957a
theosdoron <teddy.gustiaux@gmail.com>
2016-01-19 14:44:34 +0100

    Ajout des classes du modèle.

---
+++ src/Model/Exercise.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class Exercise {
+	
+	public static final String DATASTORE_LABEL = "EXERCISE";
+
+}
---
+++ src/Model/Training.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class Training {
+
+	public static final String DATASTORE_LABEL = "TRAINING";
+
+}
