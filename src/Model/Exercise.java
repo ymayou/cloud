8ac788eb230e3f8ed5eb5793d278f5cf3486950d
theosdoron <teddy.gustiaux@gmail.com>
2016-01-19 15:08:39 +0100

    Ajout de propriétés au modèle.

--- src/Model/Exercise.java
+++ src/Model/Exercise.java
@@ -4,5 +4,6 @@ public class Exercise {
 	
 	public static final String DATASTORE_LABEL = "EXERCISE";
 	public static final String TITLE_LABEL = "title";
+	public static final String DURATION_LABEL = "duration";
 
 }
--- src/Model/Training.java
+++ src/Model/Training.java
@@ -4,5 +4,6 @@ public class Training {
 
 	public static final String DATASTORE_LABEL = "TRAINING";
 	public static final String TITLE_LABEL = "title";
+	public static final String DURATION_LABEL = "duration";
 
 }
