c225a390665b06fb2c8b910c15447b59879da6c2
theosdoron <teddy.gustiaux@gmail.com>
2016-01-19 14:46:55 +0100

    Ajout des propriétés de titre.

--- src/Model/Exercise.java
+++ src/Model/Exercise.java
@@ -3,5 +3,6 @@ package Model;
 public class Exercise {
 	
 	public static final String DATASTORE_LABEL = "EXERCISE";
+	public static final String TITLE_LABEL = "title";
 
 }
--- src/Model/Training.java
+++ src/Model/Training.java
@@ -3,5 +3,6 @@ package Model;
 public class Training {
 
 	public static final String DATASTORE_LABEL = "TRAINING";
+	public static final String TITLE_LABEL = "title";
 
 }
